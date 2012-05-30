a0557665f1ba80a52ab4fec921b1b41507320ff6
Thorben Lindhauer <thorben.lindhauer@student.hpi.uni-potsdam.de>
2012-05-30 21:16:19 +0200

    work in progress commit.

--- modules/activiti-engine/src/main/java/de/unipotsdam/hpi/thorben/ppi/measure/TimeMeasure.java
+++ modules/activiti-engine/src/main/java/de/unipotsdam/hpi/thorben/ppi/measure/TimeMeasure.java
@@ -11,33 +11,40 @@ import de.unipotsdam.hpi.thorben.ppi.measure.entity.TimeMeasureValue;
 
 public class TimeMeasure extends BaseMeasure {
 
+	// TODO ersetzen durch TimeInstantConditions, die über eine evaluate-Method
+	// anhand der Execution oder einiger Event-Daten ermitteln, ob sie akutell
+	// sind
 	private ActivityImpl fromActivity;
 	private ActivityImpl toActivity;
-	
+
 	public TimeMeasure(String id) {
 		super(id);
 	}
-	
+
 	@Override
 	public void measure(InterpretableExecution execution, CommandContext context) {
 		TimeMeasureValue timeMeasureValue = new TimeMeasureValue();
 		String processInstanceId = execution.getProcessInstanceId();
 		ActivityImpl activity = (ActivityImpl) execution.getActivity();
+		
+		// TODO das ist nicht korrekt, da from und toActivity gleich sein können
+//		rot hier, da hier weiter arbeiten
 		if (activity.getId().equals(fromActivity.getId())) {
 			timeMeasureValue.setFrom(ClockUtil.getCurrentTime());
-		} else if (activity.getId().equals(toActivity.getId())){
+		} else if (activity.getId().equals(toActivity.getId())) {
 			timeMeasureValue.setTo(ClockUtil.getCurrentTime());
+			return;
 		}
 		timeMeasureValue.setMeasureId(id);
 		timeMeasureValue.setProcessInstanceId(processInstanceId);
 		new InsertTimeValueCommand(timeMeasureValue).execute(context);
 		System.out.println("executed measure");
 	}
-	
+
 	public void setFromActivity(ActivityImpl activity) {
 		this.fromActivity = activity;
 	}
-	
+
 	public ActivityImpl getFromActivity() {
 		return fromActivity;
 	}
--- modules/activiti-engine/src/main/java/de/unipotsdam/hpi/thorben/ppi/measure/entity/BaseMeasureManager.java
+++ modules/activiti-engine/src/main/java/de/unipotsdam/hpi/thorben/ppi/measure/entity/BaseMeasureManager.java
@@ -7,4 +7,13 @@ public class BaseMeasureManager extends AbstractManager {
 	public void insertTimeMeasureValue(TimeMeasureValue value) {
 		getDbSqlSession().insert(value);
 	}
+	
+//	public void updateTimeMeasureValue(TimeMeasureValue measure) {
+//		TimeMeasureValue measureEntity = findTimeMeasureById(measure.getMeasureId());
+//		value
+//	}
+//	
+//	public TimeMeasureValue findTimeMeasureById(String measureId) {
+//	    return (TimeMeasureValue) getDbSqlSession().selectOne("selectMeasureById", measureId);
+//	  }
 }
