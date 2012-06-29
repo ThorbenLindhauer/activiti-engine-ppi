c53a78de1daef1e51703e82d1e0244c2d1307e00
Thorben Lindhauer <thorben.lindhauer@student.hpi.uni-potsdam.de>
2012-06-29 17:23:42 +0200

    created a test case for parallel execution of instances with data measures.

--- modules/activiti-engine/src/test/java/de/unipotsdam/hpi/thorben/ppi/PPIParallelExecutionTest.java
+++ modules/activiti-engine/src/test/java/de/unipotsdam/hpi/thorben/ppi/PPIParallelExecutionTest.java
@@ -1,12 +1,15 @@
 package de.unipotsdam.hpi.thorben.ppi;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.test.Deployment;
 import org.junit.Assert;
 
+import de.unipotsdam.hpi.thorben.ppi.data.ComponentToDecorate;
 import de.unipotsdam.hpi.thorben.ppi.engine.PPIProcessEngine;
 import de.unipotsdam.hpi.thorben.ppi.service.PPIService;
 
@@ -14,6 +17,7 @@ public class PPIParallelExecutionTest extends AbstractPPITest {
 
 	private static final String TIME_MEASURE_DEFINITION_ID = "simpleTimeMeasure";
 	private static final String COUNT_MEASURE_DEFINITION_ID = "simpleCountMeasure";
+	private static final String DATA_MEASURE_DEFINITION_ID = "simpleDataMeasure";
 	private static final int INSTANCES = 20;
 
 	@Deployment(resources = { "de/uni-potsdam/hpi/thorben/ppi/SimpleTimeMeasure.bpmn20.xml" })
@@ -98,10 +102,48 @@ public class PPIParallelExecutionTest extends AbstractPPITest {
 		System.out.println(result);
 	}
 	
+	@Deployment(resources = { "de/uni-potsdam/hpi/thorben/ppi/SimpleAggregatedDataMeasure.bpmn20.xml" })
+	public void testAggregatedDataMeasure() throws InterruptedException {
+		RuntimeService runtime = processEngine.getRuntimeService();
+		
+		List<Thread> instanceThreads = new ArrayList<Thread>();
+		for (int i = 0; i < INSTANCES; i++) {
+			Map<String, Object> variables = new HashMap<String, Object>();
+			ComponentToDecorate dataObject = new ComponentToDecorate();
+			
+			// set the data property to a random number between 1 and 10.
+			int number = (int) ((Math.random() * 10) + 1);
+			dataObject.setNumber(number);
+			variables.put("dataObject", dataObject);
+			Thread t = createInstantiationThread(runtime, DATA_MEASURE_DEFINITION_ID, variables);
+			t.start();
+			instanceThreads.add(t);
+		}
+		
+		// wait until all threads are done
+		for (Thread t : instanceThreads) {
+			t.join();
+		}
+		
+		PPIProcessEngine engine = (PPIProcessEngine)processEngine;
+		PPIService ppiService = engine.getPPIService();
+		Number result = ppiService.calculateAggregatedMeasure("aggMeasure", DATA_MEASURE_DEFINITION_ID);
+		Assert.assertNotSame(0.0, result);
+		System.out.println(result);
+	}
+	
 	private Thread createInstantiationThread(final RuntimeService runtime, final String processDefinitionKey) {
 		return new Thread(new Runnable() {
 			public void run() {
 				runtime.startProcessInstanceByKey(processDefinitionKey);
+			}
+		});
+	}
+	
+	private Thread createInstantiationThread(final RuntimeService runtime, final String processDefinitionKey, final Map<String, Object> variables) {
+		return new Thread(new Runnable() {
+			public void run() {
+				runtime.startProcessInstanceByKey(processDefinitionKey, variables);
 			}
 		});
 	}
