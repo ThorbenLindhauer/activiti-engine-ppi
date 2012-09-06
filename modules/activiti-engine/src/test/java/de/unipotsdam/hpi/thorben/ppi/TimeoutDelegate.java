226eb5f91953f0674f4fe8ef54a690ab4ae19092
Thorben Lindhauer <thorben.lindhauer@student.hpi.uni-potsdam.de>
2012-09-06 16:46:35 +0200

    renamed the delegate class that used for the tests

--- modules/activiti-engine/src/test/java/de/unipotsdam/hpi/thorben/ppi/TimeoutDelegate.java
+++ modules/activiti-engine/src/test/java/de/unipotsdam/hpi/thorben/ppi/TimeoutDelegate.java
@@ -3,7 +3,7 @@ package de.unipotsdam.hpi.thorben.ppi;
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.JavaDelegate;
 
-public class SysoutDelegate implements JavaDelegate {
+public class TimeoutDelegate implements JavaDelegate {
 
 	public void execute(DelegateExecution execution) throws Exception {		
 		// sleep between 0 and 5 seconds
