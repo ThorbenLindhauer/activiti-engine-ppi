e687c823f6b558006e6455a7d1e85ae8dcc62bc3
Thorben Lindhauer <thorben.lindhauer@student.hpi.uni-potsdam.de>
2012-12-12 14:55:46 +0100

    added an index to the single time measure id column.

--- modules/activiti-engine/src/main/resources/org/activiti/db/drop/activiti.mysql.drop.engine.sql
+++ modules/activiti-engine/src/main/resources/org/activiti/db/drop/activiti.mysql.drop.engine.sql
@@ -1,6 +1,8 @@
 /* Copyright 2010-2012 Alfresco Software, Ltd. */
 /* Copyright 2012 Thorben Lindhauer */
 
+drop index PPI_BASE_SINGLE_TIME_MEASURE_ID on PPI_BASE_SINGLE_TIME_MEASURE;
+
 drop index ACT_IDX_EXEC_BUSKEY on ACT_RU_EXECUTION;
 drop index ACT_IDX_TASK_CREATE on ACT_RU_TASK;
 drop index ACT_IDX_IDENT_LNK_USER on ACT_RU_IDENTITYLINK;
